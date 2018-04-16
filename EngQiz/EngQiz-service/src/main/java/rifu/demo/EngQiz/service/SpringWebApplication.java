9d99d0bacb7296cd10a70bbe89854f3eceaf35a3
INNOVASOLUTIONS\Richard.Fu <richard.tzu.hao.fu@gmail.com>
2018-04-16 17:06:46 +0800

    refactor package and adjsut h2 work correctly

--- EngQiz/EngQiz-service/src/main/java/rifu/demo/EngQiz/service/SpringWebApplication.java
+++ EngQiz/EngQiz-service/src/main/java/rifu/demo/EngQiz/service/SpringWebApplication.java
@@ -2,8 +2,12 @@ package rifu.demo.engqiz.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@EnableJpaRepositories
+@EntityScan("rifu.demo.engqiz.core.entity")
 public class SpringWebApplication {
 
     public static void main(String[] args) {
