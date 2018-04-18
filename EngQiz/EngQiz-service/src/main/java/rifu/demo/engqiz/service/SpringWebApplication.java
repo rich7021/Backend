864906b05983595dc18d52912b533ab55c82ca85
INNOVASOLUTIONS\Richard.Fu <richard.tzu.hao.fu@gmail.com>
2018-04-18 16:40:46 +0800

    remove useless code

--- EngQiz/EngQiz-service/src/main/java/rifu/demo/engqiz/service/SpringWebApplication.java
+++ EngQiz/EngQiz-service/src/main/java/rifu/demo/engqiz/service/SpringWebApplication.java
@@ -3,17 +3,12 @@ package rifu.demo.engqiz.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@EnableJpaRepositories(
-        basePackages = {
-                "rifu.demo.engqiz.core.dao"
-        }
-)
+// due to dao and entity are not in the same package, so we explicitly assign the package
+@EnableJpaRepositories(basePackages = {"rifu.demo.engqiz.core.dao"})
 @EntityScan("rifu.demo.engqiz.core.entity")
-@ComponentScan({"rifu.demo.engqiz.core.dao", "rifu.demo.engqiz.service.*"})
 public class SpringWebApplication {
 
     public static void main(String[] args) {
