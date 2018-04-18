1bcdfacb35e449f5d73ab98a44d82dd09eb15ac8
INNOVASOLUTIONS\Richard.Fu <richard.tzu.hao.fu@gmail.com>
2018-04-18 16:42:50 +0800

    remove useless code

--- EngQiz/EngQiz-service/src/test/java/rifu/demo/engqiz/service/controller/QuestionControllerTest.java
+++ EngQiz/EngQiz-service/src/test/java/rifu/demo/engqiz/service/controller/QuestionControllerTest.java
@@ -3,11 +3,9 @@ package rifu.demo.engqiz.service.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import rifu.demo.engqiz.core.dto.QuestionDTO;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,17 +19,5 @@ public class QuestionControllerTest {
     public void testGetMessage() {
         List<QuestionDTO> response = questionController.findAllQuestion();
         assertEquals("return size not matched", 2, response.size());
-
-//        assertEquals("The cookie is so __________! I love it!", response.getTitle());
-//        assertEquals(4, response.getAnswers().stream().count());
-    }
-
-    private QuestionDTO generateQuestionResponseStub() {
-        QuestionDTO response = new QuestionDTO();
-        response.setTitle("The cookie is so __________! I love it!");
-        response.setAnswers(Arrays.asList(
-                "delicious", "spicy", "sour", "shit"
-        ));
-        return response;
     }
 }
