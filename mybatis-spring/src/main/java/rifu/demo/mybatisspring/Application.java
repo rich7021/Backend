d9045fa2e0ba39258d337fcfa7d98dc9085930fa
rich7021 <coconutfu@gmail.com>
2018-05-02 01:27:50 +0800

    rename mybatisConfig -> DataSource1Config

--- mybatis-spring/src/main/java/rifu/demo/mybatisspring/Application.java
+++ mybatis-spring/src/main/java/rifu/demo/mybatisspring/Application.java
@@ -3,10 +3,10 @@ package rifu.demo.mybatisspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import rifu.demo.mybatisspring.config.MybatisConfig;
+import rifu.demo.mybatisspring.config.DataSource1Config;
 
 @SpringBootApplication
-@Import(MybatisConfig.class)
+@Import(DataSource1Config.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- mybatis-spring/src/main/java/rifu/demo/mybatisspring/config/DataSource1Config.java
+++ mybatis-spring/src/main/java/rifu/demo/mybatisspring/config/DataSource1Config.java
@@ -14,33 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Qualifier("postgresDataSource")
 @MapperScan(basePackages = "rifu.demo.mybatisspring.mapper")
-public class MybatisConfig {
-
-    @Value("postgres.jdbc.url")
-    private String postgresUrl;
-    @Value("postgres.jdbc.Driver")
-    private String postgresDriverClassName;
-    @Value("postgres.jdbc.username")
-    private String postgresUsername;
-    @Value("postgres.jdbc.password")
-    private String postgresPassword;
+public class DataSource1Config {
 
     @Bean("postgresDataSource")
     @ConfigurationProperties(prefix = "postgres.jdbc")
     public DataSource getPostgresDataSource() {
-//        DataSource dataSource = new DataSource();
-////        System.out.println("url: " + dataSource.getUrl());
-//        dataSource.setUrl(postgresUrl);
-//        dataSource.setDriverClassName(postgresDriverClassName);
-//        dataSource.setUsername(postgresUsername);
-//        dataSource.setPassword(postgresPassword);
         return new DataSource();
     }
 
     @Bean
     public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
         DataSource ds = getPostgresDataSource();
-        System.out.println("url: " + ds.getUrl());
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(ds);
         return bean.getObject();
